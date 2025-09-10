f396ce3751ada36d9434312e2738ff6b0e9e3946
C.Hajduk <christian.hajduk@outlook.de>
2025-09-10 16:54:28 +0200

    change style

--- scripts/template.js
+++ scripts/template.js
@@ -6,9 +6,13 @@ function templateBookList(index) {
         <img class="imgBook" src="./assets/img/book_207114.png" alt="Abbildung eines Buchs">
         <hr>
         <section class="details">
-            <p class="price">${books[index].price}</p>
+        <div class="wrapper">
+          <p class="price">${books[index].price} €</p>
+          <div class="wrapperLike">
             <span class="like" onclick="toggleLike">❤️</span> 
             <span>${books[index].likes}</span>
+          </div>  
+        </div>  
             <p><strong>Author:</strong> ${books[index].author}</p>
             <p><strong>Erscheinungsjahr:</strong> ${books[index].publishedyear}</p>
             <p><strong>Genre:</strong> ${books[index].genre}</p>
